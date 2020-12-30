ab01e7593122b01c0b667d6fe6724edd7222c7b3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-12-30 23:30:38 +0900

    要素用コントロールを定義し、追加ボタン押下で追加するようにした。

--- SlotMassSumCalc/Controls/CalcList.xaml.cs
+++ SlotMassSumCalc/Controls/CalcList.xaml.cs
@@ -9,12 +9,23 @@ using Xamarin.Forms.Xaml;
 
 namespace SlotMassSumCalc.Controls
 {
+	/// <summary>
+	/// 計算用リスト
+	/// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CalcList : ContentView
 	{
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public CalcList()
 		{
 			InitializeComponent();
+		}
+
+		private void AddButton_Clicked(object sender, EventArgs e)
+		{
+			ListBody.Children.Add(new CalcListItem());
 		}
 	}
 }
